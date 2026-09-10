d08fe82bb1dd243bd6fe4f6b4a2f363755342032
ryanfendi <102475046+ryanfendi@users.noreply.github.com>
2026-09-10 14:45:32 +0700

    Refactor Firebase initialization and authentication code

--- firebase.js
+++ firebase.js
@@ -1,4 +1,7 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  initializeApp
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
 import {
   getFirestore
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
@@ -8,25 +11,49 @@ import {
   signInAnonymously
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
+
 const firebaseConfig = {
+
   apiKey: "AIzaSyAXGQKpJ02LBiGSVdTlrQZXjBsnHrXLAHI",
+
   authDomain: "scanbeli.firebaseapp.com",
+
   projectId: "scanbeli",
+
   storageBucket: "scanbeli.firebasestorage.app",
+
   messagingSenderId: "791591627311",
+
   appId: "1:791591627311:web:86effe90b808fe9590f4da"
+
 };
 
-const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+const app =
+  initializeApp(firebaseConfig);
+
+
+export const db =
+  getFirestore(app);
+
+
+export const auth =
+  getAuth(app);
+
 
 export async function initAnonymousAuth() {
+
   if (auth.currentUser) {
+
     return auth.currentUser;
+
   }
 
-  const result = await signInAnonymously(auth);
+
+  const result =
+    await signInAnonymously(auth);
+
+
   return result.user;
+
 }
